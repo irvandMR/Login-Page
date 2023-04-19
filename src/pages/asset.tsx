b9ee36436b45db1336d380505830f740ebbe6323
unknown <irvandirizky9@gmail.com>
2023-04-19 14:17:18 +0700

    add table

--- src/pages/asset.tsx
+++ src/pages/asset.tsx
@@ -3,13 +3,22 @@ import axios from "axios";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
+interface RecordsItem{
+    id: number
+    name : string
+    InventoryNo : string
+    A_AssetType: string
+    ManufacturedYear : number
+    A_Asset_CreateDate : string
+}
+
 
 const AssetPage = () => {
 
-    const [assets, setAsset] = useState('')
+    const [assets, setAsset] = useState<RecordsItem[]>([])
 
     const getAsset = () => {
-        // console.log(localStorage.getItem('token'))
+       
         axios.get(`/api/v1/models/a_asset`,{
             headers : {
                 Authorization : `Bearer ${localStorage.getItem('auth')}`
@@ -31,11 +40,20 @@ const AssetPage = () => {
     return (
         <>
             {assets.length > 0 && (
-            <ul>
-                {assets.map(a => (
-                    <li key={a.id}>{a.Name}</li>
-                ))}
-            </ul>
+            // <ul>
+            //     {assets.map(a => (
+            //         <li key={a.id}>
+            //             <p> aset name : {a.Name}</p>
+            //             <p> aset type : {a.A_AssetType}</p></li>
+            //     ))}
+            // </ul>
+            <DataTable value={assets} tableStyle={{ minWidth: '50rem' }}>
+                <Column field="InventoryNo" header="InventoryNo"></Column>
+                <Column field="Name" header="Name"></Column>
+                <Column field="A_AssetType" header="Type"></Column>
+                <Column field="ManufacturedYear" header="Manufactured Year"></Column>
+                <Column field="A_Asset_CreateDate" header="CreateDate"></Column>
+            </DataTable>
             )}
         </>
     )
